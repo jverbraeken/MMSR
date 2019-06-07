d05532e7d5bbced37d4f44aa4a790818506e6644
Joost <j.verbraeken@student.tudelft.nl>
2019-06-08 00:35:45 +0200

    Implement candidate pairs

--- min_hashing.py
+++ min_hashing.py
@@ -3,11 +3,10 @@ import json
 import numpy as np
 import pandas as pd
 
-
-with open("liked_recipes.json") as file:
+with open("liked_recipes_test.json") as file:
     liked_recipes = json.load(file)
 
-data = pd.read_csv("epi_r.csv")
+data = pd.read_csv("epi_r_test.csv")
 # data = data.drop(["title",
 #                   "rating",
 #                   "calories",
@@ -26,7 +25,6 @@ data = pd.read_csv("epi_r.csv")
 newdata = data.transpose()
 # newdata.to_csv("tmp.csv")
 newdata2 = newdata.as_matrix()
-# liked_recipe_ingredients = [newdata2[recipe] for recipe in liked_recipes]
 
 
 def first_nonzero(arr, axis, invalid_val=-1):
@@ -34,22 +32,37 @@ def first_nonzero(arr, axis, invalid_val=-1):
     return np.where(mask.any(axis=axis), mask.argmax(axis=axis), invalid_val)
 
 
-signatures = np.empty([24, newdata2.shape[1]], dtype=int)
-# liked_recipe_signatures = np.empty([24, len(liked_recipes)], dtype=int)
-for i in range(24):
+B = 4
+R = 6
+signatures = np.empty([B * R, newdata2.shape[1]], dtype=int)
+liked_recipe_signatures = np.empty([B * R, len(liked_recipes)], dtype=int)
+for i in range(B * R):
     np.random.seed(i)
     shuffled_data = np.copy(newdata2)
-    # shuffled_liked_recipes = [np.copy(recipe) for recipe in liked_recipe_ingredients]
+    shuffled_liked_recipes = [np.array([np.copy(newdata2[:, recipe])]).T for recipe in liked_recipes]
     np.random.shuffle(shuffled_data)
-    # for recipe in shuffled_liked_recipes:
-    #     np.random.shuffle(recipe)
+    np.random.seed(i)
+    for recipe in shuffled_liked_recipes:
+        np.random.shuffle(recipe)
     signatures[i] = first_nonzero(shuffled_data, 0)
-    # liked_recipe_signatures[i] = \
-    #     [first_nonzero(shuffled_liked_recipes[recipe], 0).item(0) for recipe in range(len(shuffled_liked_recipes))]
+    liked_recipe_signatures[i] = \
+        [first_nonzero(shuffled_liked_recipes[recipe], 0).item(0) for recipe in range(len(shuffled_liked_recipes))]
 
-buckets_per_band = [[[] for _ in range(10000)] for band in range(8)]
-for band in range(8):
+NUM_BUCKETS_PER_BAND = 10
+buckets_per_band = [[[] for _ in range(NUM_BUCKETS_PER_BAND)] for band in range(B)]
+liked_recipe_buckets_per_band = [[] for band in range(B)]
+for band in range(B):
     for i, column in enumerate(signatures.T):
-        buckets_per_band[band][hash(tuple(column[band * 3 : band * 3 + 3])) % 10000].append(i)
+        buckets_per_band[band][hash(tuple(column[band * R: band * R + R])) % NUM_BUCKETS_PER_BAND].append(i)
+    for i, signature in enumerate(liked_recipe_signatures.T):
+        liked_recipe_buckets_per_band[band].append(
+            (hash(tuple(signature[band * R: band * R + R])) % NUM_BUCKETS_PER_BAND, i))
+
+candidate_pairs = []
+for band, liked_recipe_buckets in enumerate(liked_recipe_buckets_per_band):
+    for (bucket, liked_recipe_id) in liked_recipe_buckets:
+        for candidate in buckets_per_band[band][bucket]:
+            if (liked_recipe_id, candidate) not in candidate_pairs and (candidate, liked_recipe_id) not in candidate_pairs:
+                candidate_pairs.append((liked_recipe_id, candidate))
 
 a = 5
